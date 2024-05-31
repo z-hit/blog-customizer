12e784c34784a7652ef2199a25a810b41fa85ea1
Zhenya Hitrova <jarotea@yandex.ru>
2024-05-31 15:33:50 +1000

    Slidebar opens and closes

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,23 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [open, setOpen] = useState(false);
+	function toggleOpen() {
+		setOpen(!open);
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={open} toggleIsOpen={toggleOpen} />
+			<aside
+				className={
+					!open
+						? styles.container
+						: `${styles.container} ${styles.container_open}`
+				}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
